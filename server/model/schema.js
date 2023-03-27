03bfc91690a892eca637dff5e1f3bbd7aa892483
malcolm.wilkie <malcolm.wilkie@and.digital>
2023-03-27 15:55:27 +0100

    schema fix

--- server/model/schema.js
+++ server/model/schema.js
@@ -4,7 +4,6 @@ const userSchema= new mongoose.Schema(
     {
         name: {type:String,required:true},
         email:{type:String,required:true},
-        username:{type:String,required:true},
         password:{type:String,required:true},
         googleId:{type:String,required:false}
     },
